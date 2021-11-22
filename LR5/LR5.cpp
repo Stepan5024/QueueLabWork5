4cf86ddc3881714ebd7bd74b4fbdddb77ab32aab
Степан <bokarevstepan1@yandex.ru>
2021-11-22 13:43:15 +0300

    22/11/21 перед защитой лр

--- LR5/LR5.cpp
+++ LR5/LR5.cpp
@@ -8,73 +8,77 @@
 
 typedef int Priority;
 typedef struct Node Node;
+const int CAPACITY = 25;    // Максимальный размер очереди
 
 struct Node {
     Priority data;
     Node* next;
 };
 
-void print(Node* list);
-void push(Node** plist, Priority d);
-int is_empty(Node* list);
-Priority pop(Node** list);
-Priority deleteLastNode(Node** plist);
-int* searchByKey(Node* list, int key);
-void deleteFirstByKey(Node** plist, int key);
-void insertNodeAtMiddle(Node** plist, int data, int position);
-void insertNodeAtEnd(Node** plist, int data);
+void print(Node* queue);
+void push(Node** pQueue, Priority d);
+int is_empty(Node* queue);
+Priority deleteFirstFromQueue(Node** queue);
+Priority deleteLastNode(Node** pQueue);
+void deleteFirstByPriority(Node** pQueue, int key);
+void insertForPriority(Node** pQueue, int data, int position);
+void insertAtEnd(Node** pQueue, int data);
+
+unsigned int size = 0; // текущий размер очереди
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     system("color F0");
     
-    Node* list = NULL;
-    push(&list, 0); // добавит элемент в начало
-    Priority test[] = { 1, 2, 5, 10, 30, 40, 80 };
-
+    Node* queue = NULL;
+    
+    
+    
     Node* p;
     Priority d;
+    int* Arr;
     int x = 0;
     int value = 0;
     int index = 0;
     int priority = 0;
     int maxpriority = 0;
+    Priority test[] = { 1, 2, 5, 10, 30, 40, 80 };
     
-    int* Arr;
     do
     {
-        printf("1. Инициализация очереди 9 элементами\n");
-        printf("2. Добавление в конец очереди\n");
+        printf("1. Инициализация очереди 9-ью элементами\n");
+        
         printf("3. Добавление в очередь по убывающему приоритету\n");
         printf("4. Удаление из начала очереди\n");
         printf("5. Удаление из очереди по заданному приоритету\n");
         printf("6. Очистка очереди\n");
         printf("7. Вывод на экран элементов очереди с их приоритетами и индексами\n");
-        printf("8. Удалить элемент с первым максимальным приоритетом\n");
+        
         printf("0. Выйти\n");
         printf("\nНомер операции > "); scanf_s("%d", &x);
         switch (x)
         {
         case 1: // инициализация очереди
+            push(&queue, 0); // добавит элемент в начало
             for (size_t i = 0; i < sizeof(test) / sizeof(test[0]); i++)//инициализация списка
             {
-                insertNodeAtEnd(&list, test[i]); // добавит элемент в конец очереди
-
+                insertAtEnd(&queue, test[i]); // добавит элемент в конец очереди
             }
             
-            print(list);
+            print(queue);
             break;
-        case 2: //Добавление в конец очереди
+        /*case 2:
+            //Добавление в конец очереди
             printf("Значение > "); scanf_s("%d", &value);
-            insertNodeAtEnd(&list, value); // добавит элемент в конец очереди
-            print(list);
-            break;
+            insertAtEnd(&queue, value); // добавит элемент в конец очереди
+            print(queue);
+            break;*/
         case 3: //Добавление в очередь по убывающему приоритету
             index = 0;
             printf("Значение приоритета > "); scanf_s("%d", &value);
             priority = value;
-            p = list;
+            p = queue;
             value = p->data;
             
             while(p != NULL && p->data < priority) {
@@ -82,38 +86,38 @@ int main()
                 p = p->next;
             }
             if (value > priority)
-                push(&list, priority);
+                push(&queue, priority);
             else 
-                insertNodeAtMiddle(&list, priority, ++index);
-            print(list);
+                insertForPriority(&queue, priority, ++index);
+            print(queue);
             break;
 
-        case 4: // Удаление из очереди из начала списка
-            if (!is_empty(list)) {
-                Priority d = pop(&list);
+        case 4: // Удаление из очереди из начала очереди
+            if (!is_empty(queue)) {
+                Priority d = deleteFirstFromQueue(&queue);
                 printf("Удален элемент с приоритетом %d\n", d);
             }
-            print(list);
+            print(queue);
             break;
         case 5: // Удаление из очереди по заданному приоритету
             printf("Значение > "); scanf_s("%d", &value);
             // Удаление первого вхождения элемента по ключу
-            deleteFirstByKey(&list, value);
+            deleteFirstByPriority(&queue, value);
 
             break;
         case 6: // Очистка очереди
-            while (!is_empty(list)) {
-                Priority d = pop(&list);
+            while (!is_empty(queue)) {
+                Priority d = deleteFirstFromQueue(&queue);
                 printf("Удален элемент с приоритетом %d\n", d);
             }
-            printf("Очередь пуста: %s\n", is_empty(list) ? " Да" : " Нет");
+            printf("Очередь пуста: %s\n", is_empty(queue) ? " Да" : " Нет");
             break;
         
         case 7: // Вывод на экран элементов очереди с их приоритетами и индексами
-            print(list);
+            print(queue);
             break;
         case 8: // Удалить элемент с первым максимальным приоритетом
-            p = list;
+            p = queue;
             maxpriority = p->data;
             
             index = 0;
@@ -128,8 +132,8 @@ int main()
             printf("%d - max priority ", maxpriority);
             printf("\n");
             // Удаление первого вхождения элемента по ключу
-            deleteFirstByKey(&list, maxpriority);
-            print(list);
+            deleteFirstByPriority(&queue, maxpriority);
+            print(queue);
             break;
         }
     } while (x != 0);
@@ -139,88 +143,98 @@ int main()
 }
 
 
-void insertNodeAtEnd(Node** pList, int data) // Добавить элемент в конец очереди
+/**/void insertAtEnd(Node** pQueue, int data) // Добавить элемент в конец очереди
 {
-    Node* newNode, * temp;
+    if (size < CAPACITY) {
+        Node* newNode, * temp;
 
-    newNode = (Node*)malloc(sizeof(Node));
+        newNode = (Node*)malloc(sizeof(Node));
 
-    if (newNode == NULL) {} //Не удалось выделить память
-    else
-    {
-        newNode->data = data; // значение приоритета
-        newNode->next = NULL;
-        //head списка
-        temp = *pList;
+        if (newNode == NULL) {} //Не удалось выделить память
+        else
+        {
+            newNode->data = data; // значение приоритета
+            newNode->next = NULL;
+            //head списка
+            temp = *pQueue;
 
-        // Traverse to the last node
-        while (temp != NULL && temp->next != NULL)
-            temp = temp->next;
+            // Traverse to the last node
+            while (temp != NULL && temp->next != NULL)
+                temp = temp->next;
 
-        temp->next = newNode; // Link address part
+            temp->next = newNode; // Link address part
 
-        //printf("DATA INSERTED SUCCESSFULLY\n");
+            size++;
+        }
+    }
+    else {
+        printf("В очереди максимальное кол-во элементов\n");
     }
 }
 
-void insertNodeAtMiddle(Node** plist, int data, int position) // Вставка приоритета по убывающему списку
+void insertForPriority(Node** pQueue, int data, int position) // Вставка приоритета по убывающему списку
 {
-    int i;
-    Node* newNode, * temp;
+    if (size < CAPACITY) {
+        int i;
+        Node* newNode, * temp;
 
-    newNode = (Node*)malloc(sizeof(Node));
+        newNode = (Node*)malloc(sizeof(Node));
 
-    if (newNode == NULL) {} // Не удалось выделить память
-    else
-    {
-        newNode->data = data; 
-        newNode->next = NULL;
-
-        // получить указатель на голову списка Node
-        temp = *plist;
-
-        for (i = 2; i <= position - 1; i++) // Поставить указатель до n - 1 позиции
+        if (newNode == NULL) {} // Не удалось выделить память
+        else
         {
-            temp = temp->next;
+            newNode->data = data;
+            newNode->next = NULL;
 
-            if (temp == NULL)
-                break;
+            // получить указатель на голову списка Node
+            temp = *pQueue;
+
+            for (i = 2; i <= position - 1; i++) // Поставить указатель до n - 1 позиции
+            {
+                temp = temp->next;
+
+                if (temp == NULL)
+                    break;
+            }
+
+            if (temp != NULL)
+            {
+
+                newNode->next = temp->next; // Адрес нового узла
+
+                temp->next = newNode; // Адрес узла n-1-ого
+                //Вставка приоритета успешна
+
+            }
+            else {} // Не удалось вставить приоритет
         }
-
-        if (temp != NULL)
-        {
-            
-            newNode->next = temp->next; // Адрес нового узла
-
-            temp->next = newNode; // Адрес узла n-1-ого
-            //Вставка приоритета успешна
-            
-        }
-        else {} // Не удалось вставить приоритет
+    }
+    else {
+        printf("В очереди максимальное кол-во элементов\n");
     }
 }
-Priority pop(Node** plist) { // удаляет первый элемент
+Priority deleteFirstFromQueue(Node** pQueue) { // удаляет первый элемент
 
-    Node* p = *plist;
+    Node* p = *pQueue;
     Priority res = p->data;
-    *plist = p->next;
+    *pQueue = p->next;
     free(p);
-
+    size--;
     return res;
 }
 
-Priority deleteLastNode(Node** plist) //7.  Удаление элемента из конца списка
+Priority deleteLastNode(Node** pQueue) // Удаление элемента из конца очереди
 {
     Node* toDelete, * secondLastNode;
-    if (plist == NULL)
+    if (pQueue == NULL)
     {
-        printf("List is already empty.");
+        printf("Очередь пуста");
         return 0;
     }
     else
     {
-        toDelete = *plist;
-        secondLastNode = *plist;
+        toDelete = *pQueue;
+        secondLastNode = *pQueue;
 
         /* меняем указатель до тех пор, пока не дойдем до конца */
         while (toDelete->next != NULL)
@@ -229,9 +243,9 @@ Priority deleteLastNode(Node** plist) //7.  Удаление элемента и
             toDelete = toDelete->next;
         }
 
-        if (toDelete == *plist)
+        if (toDelete == *pQueue)
         {
-            *plist = NULL;
+            *pQueue = NULL;
         }
         else
         {
@@ -241,90 +255,65 @@ Priority deleteLastNode(Node** plist) //7.  Удаление элемента и
         Priority res = toDelete->data;
         /* освобождаем память */
         free(toDelete);
+        size--;
         return res;
     }
 }
-int is_empty(Node* list) { // проверка на пустоту списка
-    return list == NULL;
+int is_empty(Node* queue) { // проверка на пустоту списка
+    return queue == NULL;
 }
-void print(Node* list) { // вывести на экран
+void print(Node* queue) { // вывести на экран
     int index = 0;
-    for (Node* p = list; p != NULL; p = p->next) {
+    for (Node* p = queue; p != NULL; p = p->next) {
         printf("[%d] = %d\n", index, p->data);
         index++;
     }
 
     printf("\n");
 }
-int* searchByKey(Node* list, int key) { // найти индекс элемента
-    int overlap = 0;
-    for (Node* p = list; p != NULL; p = p->next) {
-        if (p->data == key) {
-            overlap++;
 
-        }
-    }
-    printf("\n");
-    int index = -1;
-    int count = 0;
-    int query = 0;
-    int* Arr = new int[overlap];
-    for (Node* p = list; p != NULL; p = p->next) {
-        if (p->data == key) {
-
-            index = query;
-            count++;
-            Arr[count] = index;
-            printf("Ключ %d поиска найден. Индекс равен %d\n", key, index);
-        }
-        query++;
-    }
-    if (index == -1) {
-        printf("Ключ %d поиска не найден\n", key);
-    }
-    return Arr;
-}
-void push(Node** plist, Priority d) { // положить элемент в начало
+void push(Node** pQueue, Priority d) { // положить элемент в начало
     Node* p = (struct Node*)malloc(sizeof(Node));
     p->data = d;
-    p->next = *plist;
-    *plist = p;
+    p->next = *pQueue;
+    *pQueue = p;
+    size++;
 }
 
-void deleteFirstByKey(Node** plist, int key) // Удалить первое вхождение элемента по ключу в односвязанном списке
+void deleteFirstByPriority(Node** pQueue, int priority) // Удалить первое вхождение элемента по ключу в односвязанном списке
 {
-    struct Node* prev, * cur;
+    Node* prev, * cur;
 
-    while (*plist != NULL && (*plist)->data == key) // Проверка если голова списка Node содержит ключ удаления
+    while (*pQueue != NULL && (*pQueue)->data == priority) // Проверка если голова списка Node содержит ключ удаления
     {
         // получить указатель на голову списка Node
-        prev = *plist;
+        prev = *pQueue;
 
         // связь головного элемента с указателем начала и следующего за ним
-        *plist = (*plist)->next;
+        *pQueue = (*pQueue)->next;
 
         // Удаление предыдущей версии, так как она содержит ссылку на предыдущий узел
         free(prev);
-
+        size--;
 
         return;
     }
 
     prev = NULL;
-    cur = *plist;
+    cur = *pQueue;
 
     while (cur != NULL) // Перебор всех узлов в очереди
     {
         // Текущий узел содержит ключ
-        if (cur->data == key)
+        if (cur->data == priority)
         {
-            // Настройка ссылок для предыдущего узла
-            if (prev != NULL)
+            
+            if (prev != NULL) // Настройка ссылок для предыдущего узла
                 prev->next = cur->next;
 
-            // Удалить текущий узел
-            free(cur);
-
+            
+            free(cur); // Удалить текущий узел
+            size--;
             return;
         }
 
